8470ad5f08ca3043adb5adf43cbc65ced0d0b133
harsh-034 <harshgupta00034@gmail.com>
2025-03-06 23:50:31 +0530

    Update

--- admission/admission_form.php
+++ admission/admission_form.php
@@ -30,7 +30,7 @@ if(!isset($_SESSION['user_name'])){
 	<nav class="navbar navbar-expand-lg navbar-light">
 		<div class="container">
 		<a class="navbar-brand fw-bold d-flex align-items-center" href="#">
-    <img src="../logo.png" alt="Logo" width="200" height="40" class="d-inline-block me-2">
+   <a href="../index.html"><img src="../logo.png" alt="Logo" width="200" height="40" class="d-inline-block me-2"></a>
    
 </a><!-- logo -->
 			<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
